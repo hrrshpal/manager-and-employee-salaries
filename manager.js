71ba5c3562c140c5cb67416f32509e144c385e2e
harsh <hrrshpal@gmail.com>
2024-08-04 18:47:17 +0530

    phase 4 finished

--- manager.js
+++ manager.js
@@ -9,7 +9,25 @@ class Manager extends Employee{
     addEmployee(employee){
         this.employees.push(employee)
     }
-}
 
+    calculateBonus(multiplier){
+        const totalSalary = this.salary + this._totalSubSalary();
+        return totalSalary * multiplier;
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for (let employee of this.employees) {
+            if (employee instanceof Manager) {
+                sum += employee.salary;
+                sum += employee._totalSubSalary();
+            } else {
+                sum += employee.salary;
+            }
+        }
+        return sum;
+    }
+
+}
 
 module.exports = Manager;
