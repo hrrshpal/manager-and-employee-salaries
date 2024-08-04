97d87841a58589d5c6bad8bc4b4b9c60319b0e0e
harsh <hrrshpal@gmail.com>
2024-08-04 16:58:02 +0530

    phase 3 finished

--- employee.js
+++ employee.js
@@ -4,6 +4,10 @@ class Employee{
         this.salary = salary;
         this.title = title;
         this.manager = manager;
+
+        if(this.manager!==null){
+            manager.addEmployee(this)
+        }
     }
 }
 
